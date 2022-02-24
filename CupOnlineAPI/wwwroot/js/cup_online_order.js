5142d83be2299ff3ab606fac638c8be9485d2201
Driftinfo <Driftinfo@COREIT-DRIFTSIN>
2022-02-24 16:57:32 +0100

    fetch methods js

--- CupOnlineAPI/wwwroot/js/cup_online_order.js
+++ CupOnlineAPI/wwwroot/js/cup_online_order.js
@@ -66,9 +66,104 @@ function organizerForm(data) {
 }
 
 function newOrganizer() {
-    var clubName = document.getElementById("order_new_club_name").value;
-    var clubUrl = document.getElementById("order_new_club_url").value;
-    var clubCityId = document.getElementById("order_city").value;
-    var clubSportId = document.getElementById("order_sport").value;
+
+    let organizer = {
+        club_name: document.getElementById("order_new_club_name").value,
+        club_shortname: document.getElementById("order_new_club_name").value,
+        club_url: document.getElementById("order_new_club_url").value,
+        club_city_id: document.getElementById("order_new_club_city").value,
+        club_sport_id: document.getElementById("order_sport").value,
+    }
+
+    fetch(urlCreateOrganizer, {
+        method: "POST",
+        body: JSON.stringify(organizer),
+    headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch (err => console.log(err));
 }
 
+function newCity() {
+
+    let city = {
+        city_name: document.getElementById("order_new_club_city").value, //Add Check if city exists*************************************************************
+    }
+
+    fetch(urlCreateCity, {
+        method: "POST",
+        body: JSON.stringify(city),
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
+}
+
+function newCup() {
+    let cup = {
+        cup_club_id: document.getElementById("order_organizer").value,
+        cup_sport_id: document.getElementById("order_sport").value,
+        cup_startdate: document.getElementById("order_startdate").value,
+        cup_enddate: document.getElementById("order_enddate").value,
+        cup_name: document.getElementById("order_name").value,
+        cup_players_age: document.getElementById("order_age_text").value,
+        cup_play_place: document.getElementById("order_city")
+    }
+
+    fetch(urlCreateCup, {
+        method: "POST",
+        body: JSON.stringify(cup),
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
+}
+
+function newRegistration() {
+    let registration = {
+        message: document.getElementById("order_message").value,
+        invoiceAddress:,
+        registrationDate:,
+        payDate:,
+        orderStatus:,
+        foundType:,
+        regIp:,
+        status:,
+        payAmount:,
+    }
+
+    fetch(urlCreateCupRegistration, {
+        method: "POST",
+        body: JSON.stringify(registration),
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
+}
+
+function newCupAdmin() {
+    let cupAdmin = {
+        cup_user_username:,
+        cup_user_password:,
+        cup_user_cup_id:,
+        cup_user_rights:,
+        cup_user_name:document.getElementById("order_contact_name"),
+        cup_user_email: document.getElementById("order_contact_mail"),
+        cup_user_phone: document.getElementById("order_contact_number"),
+    }
+
+
+    fetch(urlCreateCupAdmin, {
+        method: "POST",
+        body: JSON.stringify(cupAdmin),
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
+}
+  
